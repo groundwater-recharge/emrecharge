dd912bfc21d84e3d56f656b353e2c83456aef892
stevejpurves <steve@curvenote.com>
2023-01-11 12:10:00 +0000

    relax python min version to `3.6`

--- emrecharge/setup.py
+++ emrecharge/setup.py
@@ -25,7 +25,7 @@ setuptools.setup(
         "Intended Audience :: Science/Research",
     ],
     packages=setuptools.find_packages(include=['emrecharge']),
-    python_requires='>=3.8',
+    python_requires='>=3.6',
     install_requires=[
         "rasterio",
         "shapely",
